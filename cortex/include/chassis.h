1d9e63acfb6775ffb346e524c83822ab6617dad4
itsarune <arunbsfc1@gmail.com>
2018-08-31 23:09:19 -0400

    1.0.0 Using macros to define motors and new PID-controlled movement
    - Testing pid struct in init
    - Macro replacing motor number for easier usage across programs
    - Moving key source files to main to avoid repeated declarations
    - Add some documentation to new PID functions

--- cortex/include/chassis.h
+++ cortex/include/chassis.h
@@ -1,6 +1,9 @@
 #ifndef _CHASSIS_H_
 #define _CHASSIS_H_
 
+#define leftMotor  2
+#define rightMotor 3
+
 //set speed of drive system
 void chassisSet(int left, int right);
 
--- cortex/src/encoder_pid.c
+++ cortex/src/encoder_pid.c
@@ -1,6 +1,6 @@
-#include "encoder_pid.h"
-#include "main.h"
+#include "main.h"               //include other important functions
 
+//sets the PID controller values for each instance
 void pidSet(pid_info* pid,
   double p, double i, double d,
   double motor) {
@@ -10,7 +10,11 @@ void pidSet(pid_info* pid,
     pid->motor = motor;
   }
 
-void encoderMotor(pid_info* pid, target, sensor_reading) {
+/*
+  This function uses a PID controller using information from the PID structure
+  to move the robot based on sensor-readings (typically encocder-ticks)
+*/
+void encoderMotor(pid_info* pid, int target, int sensor_reading) {
   int lastError = 0;
   int integral = 0;
   bool run = true;
@@ -22,11 +26,13 @@ void encoderMotor(pid_info* pid, target, sensor_reading) {
     integral += error;
     derivative = error - lastError;
 
-    if (error == 0 || (abs(error) > (127/2)) { integral = 0; }
+    if(error == 0 || (abs(error) > (127/2))) { integral = 0; }
 
     speed = (pid->p*error) + (pid->i*integral) + (pid->d*derivative);
 
     motorReq(pid->motor, speed);
+
+    if (error == 0 && lastError == 0) { run = false; }
 
     lastError = error;
 
@@ -38,4 +44,9 @@ void intRatio(int encoderTicks, int angle) {
   ratio = encoderTicks / angle;
 }
 
-void encoderTurn(bool turn, float angle);
+void encoderTurn(bool turn, float angle, int sensor_reading,
+    pid_info* pid, pid_info* motor2) {
+  encoderMotor(pid, (angle*ratio), sensor_reading);
+  angle *= -1;
+  encoderMotor(motor2, (angle*ratio), sensor_reading);
+}
