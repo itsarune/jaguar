cce90199c67bd1cb57582435377d1209a238d6c7
Arun <arunbsfc1@gmail.com>
2018-10-09 17:10:23 -0400

    updates from Tuesday

--- cortex/src/chassis.c
+++ cortex/src/chassis.c
@@ -2,8 +2,9 @@
 
 void chassisSet(int left, int right) { //sets the speed of the drive system
   //set drive train speeds
-  motorReq(leftMotor1, left);
-  motorReq(leftMotor2, left);
-  motorReq(rightMotor1, right);
-  motorReq(rightMotor2, right);
+  motorSet(5, left);
+  motorSet(3, left);
+  motorSet(4, right);
+  motorSet(2, right);
+  printf("%d\n", left);
 }
