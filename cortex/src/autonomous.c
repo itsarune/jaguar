ca8339cd81ff910df75a38d740dbd0d593ccf87e
helpmynamewontfi <aydan.jiwani@gmail.com>
2019-01-15 15:23:51 -0500

    pseudocode for auton routines
    will convert

--- cortex/src/autonomous.c
+++ cortex/src/autonomous.c
@@ -10,8 +10,39 @@ void myAuton() {
     printf("Autonomous done!");
     return;
   }
-  encoderMotorAutonomous(autonBackLeft, autonBackRight, -1100, -1100);
   motorReq(1, 127);
-  encoderMotorAutonomous(autonBackLeft, autonBackRight, -400, -400);
+  encoderMotorAutonomous(autonBackLeft, autonBackRight, -1500, -1500);
   motorReq(1,0);
 }
+
+
+/* AUTON ROUTINES PSEUDOCODE
+Front1:
+intake first ball
+fire first ball high
+intake second ball
+fire second ball low
+move forwards and flip flag
+turn towards center
+move to cone
+intake ball
+run intake other dir to flip cap
+
+Front2:
+
+
+Back1:
+Run forwards
+Run intake to flip cone
+turn towards back of field (l/R)
+MOVE FORWARDS
+flip cone w/balls
+go to platform
+
+Back2:
+
+
+
+
+
+*/
