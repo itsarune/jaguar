7c08bb6caf11cfc51e3415989491eb5f1cc437ff
RayhaanT <tanweer.rayhaan@gmail.com>
2019-01-29 10:19:31 -0500

    Lock right motors at x

--- cortex/src/chassis.c
+++ cortex/src/chassis.c
@@ -3,9 +3,19 @@
 void chassisSet(int left, int right) { //sets the speed of the drive system
   //motordrive systems
   motorReq(leftMotor1, left);
-  motorReq(rightMotor1, -right);
   motorReq(leftMotor2, left);
   motorReq(leftMotor3, left);
-  motorReq(rightMotor2, -right);
-  motorReq(rightMotor3, -right);
+  if(abs(right) < 90) //VALUE NEEDS TESTING WITH OPCONTROL PID
+  {
+    motorReq(rightMotor1, -right);
+    motorReq(rightMotor2, -right);
+    motorReq(rightMotor3, -right);
+  }
+  else
+  {
+    float multiplier = right > 0 ? 1 : -1;
+    motorReq(rightMotor1, 90 * multiplier);
+    motorReq(rightMotor2, 90 * multiplier);
+    motorReq(rightMotor3, 90 * multiplier);
+  }
 }
