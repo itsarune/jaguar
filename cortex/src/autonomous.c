6a08259d4c9cfc0c712f1017841021390191f55e
helpmynamewontfi <aydan.jiwani@gmail.com>
2019-01-30 13:49:59 -0500

    Update autonomous.c

--- cortex/src/autonomous.c
+++ cortex/src/autonomous.c
@@ -19,7 +19,29 @@ void myAuton(int route) {
       return;
     }
   } else if (route == 2) {
+    encoderReset(encoderRight);
+    encoderReset(encoderLeft);
+    taskCreate(motorslewing, TASK_DEFAULT_STACK_SIZE, NULL,	TASK_PRIORITY_HIGHEST);
+    int autonEndTime = millis() + 15000;
+    printf("Starting autonomous code at %d", (int)millis());
+    encoderMotorAutonomous(autonBackLeft, autonBackRight, -800, -800);
+    flagShoot();
+    encoderMotorAutonomous(autonBackLeft, autonBackRight, -1200, -1200);
+    encoderMotorAutonomous(autonBackLeft, autonBackRight, 2000, -2000);
+    encoderTurn(90);
+    encoderMotorAutonomous(autonBackLeft, autonBackRight, -1200, -1200);
+    motorReq(1, 127);
+    encoderMotorAutonomous(autonBackLeft, autonBackRight, -250, -250);
+    motorReq(1,0);
+    encoderTurn(90);
+    encoderMotorAutonomous(autonBackLeft, autonBackRight, -1600, -1600);
+
+
+
+    if ((int)millis() >= autonEndTime) {
+      printf("Autonomous done!");
       return;
+    }
   }
 }
 
